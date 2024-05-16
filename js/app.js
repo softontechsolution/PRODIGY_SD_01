e82c3ebcdbd3e08aaf242b9db0653476995a13f6
softontechsolution <softontechsolution@gmail.com>
2024-05-16 04:29:07 -0700

    adjusted

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // scripts
 
 const celsiusField = document.querySelector("#celsius");
+const kelvinField = document.querySelector("#kelvin");
+const fahrenheitField = document.querySelector("#fahrenheit");
 const degree = document.querySelector("#degree");
 const convertBtn = document.querySelector("#convert-btn");
 const tempType = document.querySelector("#temp-type");
@@ -9,6 +11,8 @@ const tempType = document.querySelector("#temp-type");
 window.addEventListener("load", () => {
     degree.value = "";
     celsiusField.innerHTML = ""; 
+    kelvinField.innerHTML = ""; 
+    fahrenheitField.innerHTML = ""; 
 })
 
 
@@ -30,13 +34,27 @@ function convertToCelsius() {
     setTimeout(() => {
         if(tempType.value === "fahrenheit") {
             const fahrenheitToCelsius = (inputValue -32) * (5/9);
+            const fahrenheitToKelvin = (inputValue -32) * (5/9) + 273;
             celsiusField.innerHTML = `${fahrenheitToCelsius.toFixed(3)} &deg;
             c`;
+            kelvinField.innerHTML = `${fahrenheitToKelvin.toFixed(3)} &deg;
+            k`;
         }
         else if(tempType.value ==="kelvin"){
-            const kelvinToCelsius = inputValue - 273.15;
+            const kelvinToCelsius = inputValue + 273.15;
+            const kelvinToFahrenheit = (inputValue - 273) * (9/5) + 32;
             celsiusField.innerHTML = `${kelvinToCelsius.toFixed(3)} &deg;
             c`;
+            fahrenheitField.innerHTML = `${kelvinToFahrenheit.toFixed(3)} &deg;
+            f`;
+        }
+        else if(tempType.value ==="celsius"){
+            const celsiusToKelvin = inputValue - 273.15;
+            const celsiusToFahrenheit = (inputValue) * (9/5) + 32;
+            kelvinField.innerHTML = `${celsiusToKelvin.toFixed(3)} &deg;
+            k`;
+            fahrenheitField.innerHTML = `${celsiusToFahrenheit.toFixed(3)} &deg;
+            f`;
         }
     }, 1200)
 }
